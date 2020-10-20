d55d88b9b89611753464b75679d31a2c56b6907d
mattdemuth <60527205+mattdemuth@users.noreply.github.com>
2020-10-20 08:47:51 -0500

    add jwt secret

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ import { check, validationResult } from 'express-validator';
 import cors from 'cors';
 import bcrypt from 'bcryptjs';
 import User from './models/User';
+import jwt from 'jsonwebtoken';
+import config from 'config';
+
 
 const app = express();
 
@@ -65,9 +68,24 @@ app.post(
 
 
                 await user.save();
-                res.send('User successfully registered');
+
+                const payload = { 
+                    user: {
+                        id: user.id
+                    }
+                };
+
+                jwt.sign(
+                    payload,
+                    config.get('jwtSecret'),
+                    { expiresIn: '10hr'},
+                    (err, token) => {
+                        if (err) throw err;
+                        res.json({ token: token});
+                    }
+                );
             }catch (error) {
-                res.status(500).send('Server error');
+                res.status(500).send('server error');
             }
         }
     }
