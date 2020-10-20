43284ce1559e43a5b3fbd867c730354131d1f8a9
mattdemuth <60527205+mattdemuth@users.noreply.github.com>
2020-10-20 08:38:34 -0500

    changes to db.js and server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import connectDatabase from './config/db';
 import { check, validationResult } from 'express-validator';
 import cors from 'cors';
+import bcrypt from 'bcryptjs';
+import User from './models/User';
 
 const app = express();
 
@@ -37,12 +39,36 @@ app.post(
         check('email', 'Please enter your email').isEmail(),
         check('password', 'Please enter your password with 6 or more characters').isLength({min: 6})
     ],
-    (req, res) => {
+    async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(422).json({errors: errors.array() });
         }else{
-            return res.send (req.body)
+        const { name, email, password } = req.body;
+        try{
+
+            let user = await User.findOne({ email: email });
+            if (user) {
+                return res
+                .status (400)
+                .json({ errors: [{ msg : 'User already exists'}] });
+            }
+
+            user = new User({
+                name: name,
+                email: email,
+                password: password
+            });
+
+                const salt = await bcrypt.genSalt(10);
+                user.password =await bcrypt.hash(password, salt);
+
+
+                await user.save();
+                res.send('User successfully registered');
+            }catch (error) {
+                res.status(500).send('Server error');
+            }
         }
     }
 );
