bee677b38387f5e66ba82ac5fad463c9506de8f1
mattdemuth <60527205+mattdemuth@users.noreply.github.com>
2020-11-03 10:23:51 -0600

    added register

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import axios from 'axios';
-
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 class App extends React.Component{
   state = {
     data:null
@@ -21,11 +21,37 @@ class App extends React.Component{
 
   render(){
     return (
-      <div className="App">
-        <header className="App-Header">
-          GoodThings
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-Header">
+            <h1>GoodThings</h1>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/register">Register</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            </ul>
+          </header>
+          <main>
+            <Route exact path="/">
+              {this.state.data}
+            </Route>
+            <Switch>
+              <Route path ='/register'>
+                Register
+              </Route>
+              <Route path="/login">
+                Login
+              </Route>
+            </Switch>
+          </main>
+        </div>
+      </Router>
     );
   }
 }
