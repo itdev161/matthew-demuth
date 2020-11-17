9349f9b3de5faf9964387c04bece3ef77805f011
mattdemuth <60527205+mattdemuth@users.noreply.github.com>
2020-11-17 11:00:32 -0600

    Created post endpoints

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ import connectDatabase from './config/db';
 import { check, validationResult } from 'express-validator';
 import cors from 'cors';
 import bcrypt from 'bcryptjs';
-import User from './models/User';
 import jwt from 'jsonwebtoken';
 import config from 'config';
+import User from './models/User';
+import Post from './models/Post';
 import auth from './middleware/auth';
 
 
@@ -159,6 +160,53 @@ const returnToken = (user, res) => {
     }
   );
 };
+
+// Post endpoints
+/**
+ * @route POST api/posts
+ * @desc Create post
+ */
+app.post(
+  '/api/posts',
+  [
+    auth,
+    [
+      check('title', 'Title text is required')
+        .not()
+        .isEmpty(),
+      check('body', 'Body text is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+    } else {
+      const { title, body } = req.body;
+      try {
+        // Get the user who created the post
+        const user = await User.findById(req.user.id);
+
+        // Create a new post
+        const post = new Post({
+          user: user.id,
+          title: title,
+          body: body
+        });
+
+        // Save to the db and return
+        await post.save();
+
+        res.json(post);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send('Server error');
+      }
+    }
+  }
+);
 
 // Connection listener
 const port = 5000;
