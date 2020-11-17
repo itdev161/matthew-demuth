90f0e1d30522de8361dce8357629f70f32ddef43
mattdemuth <60527205+mattdemuth@users.noreply.github.com>
2020-11-17 11:03:31 -0600

    created an endpoint to get a single post

--- server.js
+++ server.js
@@ -220,6 +220,26 @@ app.get('/api/posts', auth, async (req, res) => {
   }
 });
 
+/**
+ * @route GET api/posts/:id
+ * @desc Get post
+ */
+app.get('/api/posts/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Make sure the post was found
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    res.json(post);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
+  }
+});
+
 // Connection listener
 const port = 5000;
 app.listen(port, () => console.log(`Express server running on port ${port}`));
